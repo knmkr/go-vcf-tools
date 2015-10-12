08994c5678c28d1399aefbc990104bdc9db55a9e
Kensuke Numakura <knmkr3gma@gmail.com>
2015-10-12 19:35:03 +0900

    add option --keep-id/--keep-ids/--keep-index

--- vcf-subset.go
+++ vcf-subset.go
@@ -6,11 +6,20 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"flag"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Printf("Usage: %s ID,ID,...(commna delimited sample IDs to be kept) < in.vcf > out.vcf\n", os.Args[0])
+	arg_keep_id := flag.String("keep-id", "", "A sample ID to be kept. E.g., NA00001")
+	arg_keep_ids := flag.String("keep-ids", "", "Path to a file of sample IDs to be kept. Each line contains one sample ID.")
+	arg_keep_index := flag.String("keep-index", "", "An index of sample ID field to be kept. E.g., to keep 1st sample, set: 0")
+	flag.Parse()
+
+	if len(os.Args) <=2 || len(os.Args) > 4 {
+		fmt.Fprintln(os.Stderr, "Set only one of --keep-id/--keep-ids/--keep-index")
+		fmt.Fprintln(os.Stderr)
+		flag.Usage()
 		os.Exit(0)
 	}
 
@@ -36,14 +45,40 @@ func main() {
 	}
 
 	// Get indices of sample IDs to be kept
-	keep_ids := strings.Split(os.Args[1], ",")
-	var keep_idxs []int
-	for i := range keep_ids {
-		for j := range sample_ids {
-			if keep_ids[i] == sample_ids[j] {
-				keep_idxs = append(keep_idxs, j)
+	keep_ids := []string{}
+	keep_idxs := []int{}
+
+	if *arg_keep_id != "" || *arg_keep_ids != "" {
+		if *arg_keep_id != "" {
+			// A sample ID to be kept. E.g., NA00001
+			keep_ids = append(keep_ids, *arg_keep_id)
+		} else {
+			// Path to a file of sample IDs to be kept. Each line contains one sample ID.
+			f, err := os.Open(*arg_keep_ids)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				line := scanner.Text()
+				keep_ids = append(keep_ids, line)
 			}
 		}
+
+		for i := range keep_ids {
+			for j := range sample_ids {
+				if keep_ids[i] == sample_ids[j] {
+					keep_idxs = append(keep_idxs, j)
+					break
+				}
+			}
+		}
+	} else if *arg_keep_index != "" {
+		// An index of sample ID field to be kept. E.g., to keep 1st sample, set: 0
+		_keep_idx, _  := strconv.Atoi(*arg_keep_index)
+		keep_idxs = append(keep_idxs, _keep_idx)
 	}
 
 	fmt.Println(strings.Join(subset(sample_ids, keep_idxs), "\t"))
