dd5f373247851cb2f3bafd3014f85c8cad727491
Kensuke Numakura <knmkr3gma@gmail.com>
2015-12-16 12:52:42 +0900

    -oerwrite and -strict option

--- vcf-fill-rsids.go
+++ vcf-fill-rsids.go
@@ -17,11 +17,27 @@ import (
 )
 
 func main() {
-	arg_bucket := flag.String("bucket", "", "Mappings of chrom/pos on reference genome. E.g., b142_SNPChrPosOnRef_105.bcp.gz")
-	arg_setup := flag.Bool("setup", false, "Setup local db.")
+	arg_bucket    := flag.String("bucket", "", "Mappings of chrom/pos on reference genome. E.g., b142_SNPChrPosOnRef_105.bcp.gz")
+	arg_setup     := flag.Bool("setup", false, "Setup local db.")
+	arg_overwrite := flag.Bool("overwrite", false, "Overwrite rs ids if already exist in vcf. However, for loci not in local db, original records will be kept.")
+	arg_strict    := flag.Bool("strict", false, "Along with '-overwrite' option, for loci not in local db will be filled as '.'")
 	flag.Parse()
 
-	if len(os.Args) <=2 || len(os.Args) > 4 {
+	if len(os.Args) <=2 {
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "[FATAL] too few arguments")
+		fmt.Fprintln(os.Stderr)
+		flag.Usage()
+		os.Exit(0)
+	} else if ! *arg_overwrite && *arg_strict {
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "[FATAL] -strict option is only effective with -overwrite option")
+		fmt.Fprintln(os.Stderr)
+		flag.Usage()
+		os.Exit(0)
+	} else if *arg_bucket == "" {
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "[FATAL] -bucket is required")
 		fmt.Fprintln(os.Stderr)
 		flag.Usage()
 		os.Exit(0)
@@ -60,8 +76,14 @@ func main() {
 			map_reader := bufio.NewReaderSize(gz, 128*1024)
 			map_line, err := lib.Readln(map_reader)
 			for err == nil {
+				// chrom/pos to rs id mapping resource file ([TAB] delimited)
+				//
+				// | rs id  | chrom  | pos    |
+				// |--------|--------|--------|
+				// |  xxxxx |     xx |  xxxxx |
+				//
 				records := strings.Split(map_line, "\t")
-				rsId := records[0]
+				rsId := strings.Replace(records[0], "rs", "", 1)
 				rsChr := records[1]
 				rsPos, _ := strconv.ParseInt(records[2], 10, 64)
 
@@ -125,11 +147,17 @@ func main() {
 		snpId := records[2]
 
 		rsIdFound := pattern.FindStringSubmatch(snpId)
-		if rsIdFound  != nil {
-			// skip
+		// Skip or fill rs id. Switch by '-overwrite' option.
+		//
+		// | input     | overwrite = t | overwrite = f |
+		// |-----------|---------------|---------------|
+		// | "rsxxxx"  | fill          | skip          |
+		// | "."       | fill          | fill          |
+		if rsIdFound != nil && ! *arg_overwrite {
+			// Skip
 			fmt.Println(line)
-		} else {
-			// fill
+		} else if (rsIdFound != nil && *arg_overwrite) || rsIdFound == nil {
+			// Fill
 			result := []string{}
 			result = append(result, records[0:2]...)
 
@@ -142,9 +170,17 @@ func main() {
 				val := bucket.Get(lib.Itob(lib.ChrPos(chrom, pos)))
 
 				if val != nil {
+					// Fill rs id if locus is found.
 					result = append(result, "rs" + string(val))
 				} else {
-					result = append(result, ".")
+
+					if *arg_strict {
+						// Fill '.' if locus in not found ('-strict' option).
+						result = append(result, ".")
+					} else {
+						// Keep original record (including '.') if locus is not found.
+						result = append(result, snpId)
+					}
 				}
 
 				return nil
